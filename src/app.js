824d2bfa22aac795081eaa59ceb9d61a34345449
snehasaxena04srdt-cmd <snehasaxena04.srdt@gmail.com>
2026-09-19 17:55:25 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express=require("express");
+const  authRouters= require("./routers/auth.routers");
+
 
 const app=express();
+app.use(express.json());
+
+app.use("/api/auth", authRouters)
 
 module.exports=app;
